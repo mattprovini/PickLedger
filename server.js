db13812f673513c7d75b0bcabe1848f713bec947
Matt <provinimatthew@gmail.com>
2026-04-21 13:52:46 -0400

    Added backend to GitHub

---
+++ server.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const User = require('./models/User');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const session = require('express-session');
+
+const app = express();
+
+app.use(cors({
+	origin: 'http://192.168.56.102:3000',
+	credentials: true
+}));
+
+app.use(express.json());
+app.use(helmet());
+app.use(session({
+	secret: 'secretkey',
+	resave: false,
+	saveUninitialized: false,
+	cookie: {
+		httpOnly: true
+	}
+}));
+
+// Could not get to work? 
+const loginLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 10,
+	message: { message: "Too many login attempts, try again later"}
+});
+
+// Connect to mongo
+mongoose.connect('mongodb://192.168.56.103:27017/pickledger');
+
+// Log connection
+mongoose.connection.once('open', () => {
+	console.log("Connected to MongoDB");
+});
+
+// register
+app.post('/api/register', async (req, res) => {
+	let {username, email, password } = req.body;
+
+	// Trim down input to eliminate SQL injection risk
+	username = String(username).trim();
+	email = String(email).trim();
+	password = String(password);
+
+	// Test input
+	if(!username || !email || !password) {
+		return res.status(400).json({ message: "All fields are required" });
+	}
+	
+	if (username.length < 3 || username.length > 20) {
+		return res.status(400).json({ message: "Username length invalid" });
+	}
+	if (password.length < 6) {
+		return res.status(400).json({ message: "Password too short" });
+	}
+
+	// Hash password
+	const hashedPassword = await bcrypt.hash(password, 10);
+
+	const user = new User({
+		username,
+		email,
+		password: hashedPassword
+	});
+
+	await user.save();
+	
+	res.json({ message: "User created" });
+});
+
+// Login
+app.post('/api/login', async (req, res) => {
+	const { username, password } = req.body;
+
+	const user = await User.findOne({ username });
+
+	if (!user) return res.json({ message: "User not found" });
+
+	const valid = await bcrypt.compare(password, user.password);
+
+	if(!valid) return res.json({ message: "Invalid password" });
+
+	// Store session
+	req.session.userId = user._id;
+
+	res.json({ message: "Login successful" });
+});
+
+// Logout feature
+app.get('/api/logout', (req, res) => {
+	req.session.destroy(() => {
+		res.json({ message: "Logged out" });
+	});
+});
+
+app.get('/api/check-auth', (req, res) => {
+	if (req.session.userId) {
+		res.json({ loggedIn: true });
+	} else {
+		res.json({ loggedIn: false });
+	}
+});
+
+// Log connections
+app.get('/api/test', (req, res) => {
+	res.json({ message: "Backend working " });
+});
+
+app.listen(3000, '0.0.0.0', () => {
+	console.log("Server running on port 3000");
+});
